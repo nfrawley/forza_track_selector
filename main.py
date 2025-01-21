e271979185cb7b06516b90af26275b4bf21daaea
Noah Frawley <noah.frawley@proton.me>
2025-01-20 17:49:29 -0800

    Continue moving selections off the main app to its own class

--- main.py
+++ main.py
@@ -14,16 +14,9 @@ class App(customtkinter.CTk):
         self.logs = utilities.Logs(app_name="Forza Track Selector")
         # Init variables
         self.file = 'Settings.ini'
-        self.settings = {
-            'APPEARANCE': 'System',
-            'UPGRADE_TRACKER': 'Yes',
-            'UPGRADE_INTERVAL': '3',
-            'NO_REPEAT': 'Yes'
-        }
         self.session_history = [
             {'location': "", "layout": []}
         ]
-        self.total_races = 0
 
         # Define the GUI
         self.frame = customtkinter.CTkFrame(self)
@@ -37,9 +30,9 @@ class App(customtkinter.CTk):
         self.stats_frame.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
         self.buttons_frame = customtkinter.CTkFrame(self.frame)
         self.buttons_frame.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
-        self.random_button = customtkinter.CTkButton(self.buttons_frame, text="Random Track", command=lambda: [self.roll.track(), self.update_total()])
+        self.random_button = customtkinter.CTkButton(self.buttons_frame, text="Random Track", command=lambda: [self.roll.track(), self.roll.weather(), self.roll.time(), self.history.update_total()])
         self.random_button.grid(row=0, column=0, padx=10, pady=10)
-        self.reset_button = customtkinter.CTkButton(self.buttons_frame, text="Reset", command=self.reset_history)
+        self.reset_button = customtkinter.CTkButton(self.buttons_frame, text="Reset", command=lambda: [self.history.reset()])
         self.reset_button.grid(row=1, column=0, padx=10, pady=10)
         self.location_label = customtkinter.CTkLabel(self.options_frame, text="Location: ")
         self.location_label.grid(row=2, column=0, padx=10, pady=10)
@@ -59,63 +52,26 @@ class App(customtkinter.CTk):
         self.time_text.grid(row=5, column=1, padx=10, pady=10)
         self.total_races_label = customtkinter.CTkLabel(self.stats_frame, text="Total Races:")
         self.total_races_label.grid(row=1, column=0, padx=10, pady=10)
-        self.total_races_text = customtkinter.CTkLabel(self.stats_frame, text=self.total_races)
+        self.total_races_text = customtkinter.CTkLabel(self.stats_frame, text="0")
         self.total_races_text.grid(row=1, column=1, padx=10, pady=10)
 
+        # Init classes
         self.settings = Settings(self)
-        self.roll = Roll(self)
         self.history = History(self)
+        self.roll = Roll(self)
         self.settings.validate()
-
-    def history_check(self, location: str, layout: str) -> bool:
-        """Check if the location and layout exists in the session history"""
-        for track in self.session_history:
-            if track['location'] == location and layout in track['layout']:
-                self.logs.debug(f"Exists in  history: {location} - {layout}")
-                return True
-        return False
-
-    def reset_history(self):
-        """Reset the history of tracks and layouts"""
-        self.logs.info("RESETTING HISTORY!")
-        self.logs.debug("But First, here's the history:")
-        self.logs.debug(self.session_history)
-        self.session_history = [
-            {'location': "", "layout": []}
-        ]
-        self.total_races = 0
-        self.total_races_text.configure(text=self.total_races)
-        self.location_text.configure(text="")
-        self.layout_text.configure(text="")
-        self.weather_text.configure(text="")
-        self.time_text.configure(text="")
-
-    def random_weather(self):
-        """Randomly select a weather condition"""
-        self.logs.debug("Rolling for weather..")
-        selected_weather = options.weather_list[random.randint(0, len(options.weather_list) - 1)]
-        self.logs.debug(f"Selected weather: {selected_weather}")
-        self.weather_text.configure(text=selected_weather)
-
-    def random_time(self):
-        """Randomly select a time of day"""
-        self.logs.debug("Rolling for time..")
-        selected_time = options.time_of_day[random.randint(0, len(options.time_of_day) - 1)]
-        self.logs.debug(f"Selected time: {selected_time}")
-        self.time_text.configure(text=selected_time)
-
-    def update_total(self):
-        """Add 1 to the total races count"""
-        self.total_races +=1
-        self.logs.debug(f"Updating total races to {self.total_races}")
-        self.total_races_text.configure(text=self.total_races)
 
 class Settings:
     """Class to manage the app settings"""
     def __init__(self, app):
         self.logs = app.logs
-        self.settings = app.settings
         self.file = app.file
+        self.settings = {
+            'APPEARANCE': 'System',
+            'UPGRADE_TRACKER': 'Yes',
+            'UPGRADE_INTERVAL': '3',
+            'NO_REPEAT': 'Yes'
+        }
     def validate(self):
         """Check if the settings file exists and contains the required settings"""
         # Check if the settings file exists
@@ -147,15 +103,38 @@ class Settings:
                     self.logs.error(f"Error writing {key} = {value} to {self.file}")
                     self.logs.error(a['result'])
 
+    def modify(self, key: str, value: str):
+        """Modify a setting in the settings file"""
+        x = utilities.Ini.write(self.file, 'APP', key, value)
+        if x['success']:
+            self.logs.debug(f"Wrote {key} = {value} to {self.file}")
+        else:
+            self.logs.error(f"Error writing {key} = {value} to {self.file}")
+            self.logs.error(x['result'])
+
+    def read(self, key: str) -> dict:
+        """Read a setting from the settings file"""
+        x = utilities.Ini.load(self.file, 'APP', key)
+        if x['success']:
+            self.logs.debug(f"Read {key} = {x['result']} from {self.file}")
+        else:
+            self.logs.error(f"Error reading {key} from {self.file}")
+            self.logs.error(x['result'])
+        return x
+
 class Roll:
     """Class to manage randomization"""
     def __init__(self, app):
         self.logs = app.logs
+        self.history = app.history
         self.settings = app.settings
-        self.history = app.session_history
+        self.session_history = app.session_history
         self.layout_text = app.layout_text
         self.location_text = app.location_text
+        self.weather_text = app.weather_text
+        self.time_text = app.time_text
         self.app = app
+
     def track(self):
         """Randomly select a location and layout"""
         number_of_locations = len(options.tracks_list)
@@ -186,15 +165,16 @@ class Roll:
         selected_layout_name = options.tracks_list[selected_location_int]['layout'][selected_layout_int]
         self.logs.info(f"Selected layout: {selected_layout_name}")
 
-        # If no-repeat is set, add to the list of used layouts
+        # Check if no-repeat is enabled, If no-repeat is set, add to the list of used layouts
+        x = self.settings.read('NO_REPEAT')
         if x['success']:
             if x['result'] == 'Yes':
                 # Check if this location was just used, re-roll if required
-                if App.history_check(self.app, selected_location_name, selected_layout_name):
+                if self.history.check(selected_location_name, selected_layout_name):
                     self.logs.debug(f"{selected_location_name}: {selected_layout_name} exists in history. re-rolling..")
                     self.track()
                 else:
-                    self.app.history.add(selected_location_name, selected_layout_name)
+                    self.history.add(selected_location_name, selected_layout_name)
             elif x['result'] == 'No':
                 self.logs.debug("No-repeat not enabled. Not adding to history list.")
         elif not x['success']:
@@ -203,8 +183,21 @@ class Roll:
 
         self.layout_text.configure(text=selected_layout_name)
         self.location_text.configure(text=selected_location_name)
-        self.app.random_weather()
-        self.app.random_time()
+
+    def weather(self):
+        """Randomly select a weather condition"""
+        self.logs.debug("Rolling for weather..")
+        selected_weather = options.weather_list[random.randint(0, len(options.weather_list) - 1)]
+        self.logs.debug(f"Selected weather: {selected_weather}")
+        self.weather_text.configure(text=selected_weather)
+
+    def time(self):
+        """Randomly select a time of day"""
+        self.logs.debug("Rolling for time..")
+        selected_time = options.time_of_day[random.randint(0, len(options.time_of_day) - 1)]
+        self.logs.debug(f"Selected time: {selected_time}")
+        self.time_text.configure(text=selected_time)
+
 
 class History:
     """Class to manage the session history"""
@@ -212,15 +205,50 @@ class History:
         self.logs = app.logs
         self.settings = app.settings
         self.history = app.session_history
+        self.total_races = 0
+        self.total_races_text = app.total_races_text
+        self.layout_text = app.layout_text
+        self.location_text = app.location_text
+        self.weather_text = app.weather_text
+        self.time_text = app.time_text
+
     def add(self, location: str, layout: str) -> dict:
         """Add the location and layout to the session history"""
         self.logs.debug("Starting loop")
         for track in self.history:
             if track['location'] == location:
                 self.logs.debug(f"{track} found")
-                if not layout in track['layout']:
+                if layout not in track['layout']:
                     track['layout'].append(layout)
                     self.logs.debug(f"{track} added")
-            else:
-                self.history.append({'location': location, 'layout': [layout]})
+                return
+        self.history.append({'location': location, 'layout': [layout]})
+        self.logs.debug(f"New track added: {location} - {layout}")
+
+    def update_total(self):
+        """Update the total races in session history"""
+        self.total_races +=1
+        self.logs.debug(f"Updating total races to {self.total_races}")
+        self.total_races_text.configure(text=self.total_races)
+
+    def reset(self):
+        """Reset the history of tracks and layouts"""
+        self.logs.info("RESETTING HISTORY!")
+        self.history = []
+        self.total_races = 0
+        self.total_races_text.configure(text=self.total_races)
+        self.location_text.configure(text="")
+        self.layout_text.configure(text="")
+        self.weather_text.configure(text="")
+        self.time_text.configure(text="")
+
+    def check(self, location: str, layout: str) -> bool:
+        """Check if the location and layout exists in the session history"""
+        for track in self.history:
+            if track['location'] == location and layout in track['layout']:
+                self.logs.debug(f"Exists in  history: {location} - {layout}")
+                return True
+        return False
+
+
 App().mainloop()
